6adb607538107637135ba07d240027c1b159e495
Greg Taube <gregjotau@gmail.com>
2023-01-24 09:58:17 +0100

    almost works

--- src/main/java/com/example/testtemplate/Question.java
+++ src/main/java/com/example/testtemplate/Question.java
@@ -1,11 +1,25 @@
 package com.example.testtemplate;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.persistence.*;
 
 import java.util.List;
 
 @Entity
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonIgnoreProperties({"colsize",
+        "question_type",
+        "answered",
+        "already_answered",
+        "server_saved",
+        "user_answer",
+        "user_answer_array",
+        "question_video_images",
+        "answerbit"
+})
 public class Question {
 
     @Id
@@ -13,54 +27,33 @@ public class Question {
 
     private String question;
 
-    @JsonProperty("question_type")
-    private int questionType;
-
     @OneToMany(cascade = CascadeType.ALL)
     private List<Answer> answers;
 
-    @Embedded
-    private Answerbit answerbit;
-
     @ElementCollection
-    @JsonProperty("correct_answer_array")
     private List<Integer> correctAnswerArray;
 
     private String chapter;
 
-    @JsonProperty("chapter_id")
     private int chapterId;
 
-    @JsonProperty("subchapter_id")
     private int subchapterId;
 
-    @JsonProperty("course_mapped")
     private boolean courseMapped;
 
     private String explanation;
 
-    @JsonProperty("explanation_active")
     private int explanationActive;
 
     private int calculator;
 
-    @JsonProperty("user_answer")
-    private int userAnswer;
-
-    @ElementCollection
-    private List<Integer> userAnswerArray;
-
     private int questionImageId;
 
-    private String IMAGE_NORMAL;
+    @JsonProperty("IMAGE_NORMAL")
+    private String image_normal;
 
-    private String IMAGE_LARGE;
-
-    private boolean answered;
-
-    private boolean alreadyAnswered;
-
-    private boolean serverSaved;
+    @JsonProperty("IMAGE_LARGE")
+    private String image_large;
 
     private int explanationImageId;
 
@@ -70,14 +63,34 @@ public class Question {
 
     private int videoId;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "question_question_video_image",
-            joinColumns = @JoinColumn(name = "question_id"),
-            inverseJoinColumns = @JoinColumn(name = "question_video_image_id"))
-    private List<QuestionVideoImage> questionVideoImages;
+    @JsonProperty("question_video_images.thumbnail_295")
+    private String questionVideo;
 
     private String imageModifiers;
 
+    public String getImage_normal() {
+        return image_normal;
+    }
+
+    public void setImage_normal(String image_normal) {
+        this.image_normal = image_normal;
+    }
+
+    public String getImage_large() {
+        return image_large;
+    }
+
+    public void setImage_large(String image_large) {
+        this.image_large = image_large;
+    }
+
+    public String getQuestionVideo() {
+        return questionVideo;
+    }
+
+    public void setQuestionVideo(String questionVideo) {
+        this.questionVideo = questionVideo;
+    }
 
     public Long getId() {
         return id;
@@ -95,13 +108,6 @@ public class Question {
         this.question = question;
     }
 
-    public int getQuestionType() {
-        return questionType;
-    }
-
-    public void setQuestionType(int questionType) {
-        this.questionType = questionType;
-    }
 
     public List<Answer> getAnswers() {
         return answers;
@@ -111,13 +117,6 @@ public class Question {
         this.answers = answers;
     }
 
-    public Answerbit getAnswerbit() {
-        return answerbit;
-    }
-
-    public void setAnswerbit(Answerbit answerbit) {
-        this.answerbit = answerbit;
-    }
 
     public List<Integer> getCorrectAnswerArray() {
         return correctAnswerArray;
@@ -183,23 +182,6 @@ public class Question {
         this.calculator = calculator;
     }
 
-    public int getUserAnswer() {
-        return userAnswer;
-    }
-
-    public void setUserAnswer(int userAnswer) {
-        this.userAnswer = userAnswer;
-    }
-
-    public List<Integer> getUserAnswerArray() {
-        return userAnswerArray;
-    }
-
-    public void setUserAnswerArray(List<Integer> userAnswerArray) {
-        this.userAnswerArray = userAnswerArray;
-    }
-
-
 
     public int getQuestionImageId() {
         return questionImageId;
@@ -209,44 +191,20 @@ public class Question {
         this.questionImageId = questionImageId;
     }
 
-    public String getIMAGE_NORMAL() {
-        return IMAGE_NORMAL;
+    public String getImageNormal() {
+        return image_normal;
     }
 
-    public void setIMAGE_NORMAL(String IMAGE_NORMAL) {
-        this.IMAGE_NORMAL = IMAGE_NORMAL;
+    public void setImageNormal(String IMAGE_NORMAL) {
+        this.image_normal = IMAGE_NORMAL;
     }
 
-    public String getIMAGE_LARGE() {
-        return IMAGE_LARGE;
+    public String getImageLarge() {
+        return image_large;
     }
 
-    public void setIMAGE_LARGE(String IMAGE_LARGE) {
-        this.IMAGE_LARGE = IMAGE_LARGE;
-    }
-
-    public boolean isAnswered() {
-        return answered;
-    }
-
-    public void setAnswered(boolean answered) {
-        this.answered = answered;
-    }
-
-    public boolean isAlreadyAnswered() {
-        return alreadyAnswered;
-    }
-
-    public void setAlreadyAnswered(boolean alreadyAnswered) {
-        this.alreadyAnswered = alreadyAnswered;
-    }
-
-    public boolean isServerSaved() {
-        return serverSaved;
-    }
-
-    public void setServerSaved(boolean serverSaved) {
-        this.serverSaved = serverSaved;
+    public void setImageLarge(String IMAGE_LARGE) {
+        this.image_large = IMAGE_LARGE;
     }
 
     public int getExplanationImageId() {
@@ -279,14 +237,6 @@ public class Question {
 
     public void setVideoId(int videoId) {
         this.videoId = videoId;
-    }
-
-    public List<QuestionVideoImage> getQuestionVideoImages() {
-        return questionVideoImages;
-    }
-
-    public void setQuestionVideoImages(List<QuestionVideoImage> questionVideoImages) {
-        this.questionVideoImages = questionVideoImages;
     }
 
     public String getImageModifiers() {
--- src/main/resources/db/migration/V7__add_quiz_tables.sql
+++ src/main/resources/db/migration/V7__add_quiz_tables.sql
@@ -2,7 +2,6 @@ CREATE TABLE question
 (
     id                   BIGINT PRIMARY KEY,
     question             TEXT    NOT NULL,
-    question_type        INT     NOT NULL,
     chapter              TEXT    NOT NULL,
     chapter_id           INT     NOT NULL,
     subchapter_id        INT     NOT NULL,
@@ -10,14 +9,10 @@ CREATE TABLE question
     explanation          TEXT    NOT NULL,
     explanation_active   INT     NOT NULL,
     calculator           INT     NOT NULL,
-    user_answer          INT     NOT NULL,
     question_image_id    INT     NOT NULL,
     image_id             INT     NOT NULL,
     image_normal         TEXT    NOT NULL,
     image_large          TEXT    NOT NULL,
-    answered             BOOLEAN NOT NULL,
-    already_answered     BOOLEAN NOT NULL,
-    server_saved         BOOLEAN NOT NULL,
     explanation_image_id INT,
     explanation_video_id INT,
     correct_answer       INT     NOT NULL,
@@ -38,34 +33,9 @@ CREATE TABLE question_answers
     answer_id   BIGINT NOT NULL REFERENCES answer (id)
 );
 
-CREATE TABLE answerbit
-(
-    question_id BIGINT NOT NULL REFERENCES question (id),
-    bitstr      INT    NOT NULL,
-    type        TEXT   NOT NULL
-);
 
 CREATE TABLE correct_answer_array
 (
     question_id BIGINT NOT NULL REFERENCES question (id),
     value       INT    NOT NULL
-);
-
-CREATE TABLE user_answer_array
-(
-    question_id BIGINT NOT NULL REFERENCES question (id),
-    value       INT    NOT NULL
-);
-
-CREATE TABLE question_video_image
-(
-    id          BIGSERIAL PRIMARY KEY,
-    question_id BIGINT NOT NULL REFERENCES question (id),
-    src         TEXT   NOT NULL
-);
-
-CREATE TABLE question_question_video_image
-(
-    question_id             BIGINT NOT NULL REFERENCES question (id),
-    question_video_image_id BIGINT NOT NULL REFERENCES question_video_image (id)
 );
