a2ee146e525f2250c16862fd0cfa696a08dbea6e
Greg Taube <gregjotau@gmail.com>
2023-01-24 17:54:59 +0100

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.postgresql:postgresql")
-	implementation ("org.springframework.boot:spring-boot-starter-actuator")
+//	implementation ("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
