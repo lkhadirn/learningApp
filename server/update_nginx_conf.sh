8deda581abb85a54ee583c3a5d4e4a423b18725f
Greg Taube <gregjotau@gmail.com>
2023-01-24 20:36:08 +0100

    configs

--- server/update_nginx_conf.sh
+++ server/update_nginx_conf.sh
@@ -1,1 +1,4 @@
+
+
+# This is the default Nginx file, it includes other under conf.d/...
 rsync --rsync-path="sudo rsync" nginx.conf ubuntu@oslo1:/etc/nginx/
---
+++ server/update_nginx_teoriproven_conf.sh
@@ -0,0 +1,1 @@
+rsync --rsync-path="sudo rsync" teoriproven.conf ubuntu@oslo1:/etc/nginx/conf.d/
