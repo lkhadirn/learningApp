018a0d23a476619e8bc35cdffcf10c50f4cb5902
Greg Taube <gregjotau@gmail.com>
2023-01-25 08:33:35 +0100

    f

--- src/main/java/com/example/testtemplate/learningApp.java
+++ src/main/java/com/example/testtemplate/learningApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HRApplication {
+public class learningApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HRApplication.class, args);
+		SpringApplication.run(learningApp.class, args);
 	}
 
 }
