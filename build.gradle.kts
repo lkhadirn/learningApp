7480caa90c192195d109de83d2746344631635fe
Greg Taube <gregjotau@gmail.com>
2023-01-27 10:09:02 +0100

    clean

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     java
     id("org.springframework.boot") version "3.0.1"
     id("io.spring.dependency-management") version "1.1.0"
-    id("org.graalvm.buildtools.native") version "0.9.18"
     id("org.flywaydb.flyway") version "9.8.1"
 }
 
@@ -39,15 +38,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.flywaydb:flyway-core")
     implementation("org.postgresql:postgresql")
-//	implementation ("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation ("org.jsoup:jsoup:1.15.3")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-//	implementation("org.springframework.session:spring-session-core")
-//	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
-//	testImplementation("org.springframework.security:spring-security-test")
-//	implementation("org.springframework.boot:spring-boot-starter-security")
 }
 
 
